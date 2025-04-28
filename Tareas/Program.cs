c1753e5d763412d34820a6c4d0eebcab98ec5f10
dan1dlm <daniel01mamani01@gmail.com>
2025-04-28 06:39:28 -0300

    ejercicio 1 realizado

---
+++ Tareas/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text.Json;
+using System.Net.Http;
+using espacioTarea;
+
+HttpClient client = new HttpClient();
+
+var url = "https://jsonplaceholder.typicode.com/todos/";
+HttpResponseMessage response = await client.GetAsync(url);
+response.EnsureSuccessStatusCode();
+string responseBody = await response.Content.ReadAsStringAsync();
+List<Tarea> listTAREA = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+
+var tareasOrdenadas = listTAREA.OrderBy(t => t.completed).ToList();
+
+foreach(var tr in listTAREA){
+    Console.WriteLine("------------------");
+    Console.WriteLine($"UserID: {tr.userId}'\nid: {tr.id}\ntitle: {tr.title}\ncompleted: {tr.completed}");
+}
+
+var opciones = new JsonSerializerOptions{
+    WriteIndented = true
+};
+
+string ListaJsoneada = JsonSerializer.Serialize(tareasOrdenadas, opciones);
+
+string ruta = "tareas.json";
+
+if(File.exists(ruta)){
+    File.AppendAlltext(ruta, ",", ListaJsoneada);
+    Console.WriteLine("Contenido agreagado al archivo existente.");
+}else{
+    File.WriteAllText(ruta, "[" + ListaJsoneada + "]");
+    Console.WriteLine("Archivo creado como tiene que ser");
+}
---
+++ Tareas/Tarea.cs
@@ -0,0 +1,14 @@
+//CLASE OBJETOS DE API
+namespace espacioTarea
+{
+    public class Tarea
+    {
+        public int userId { get; set; }
+        public int id { get; set;}
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+
+}
+
+
